aa0dc53806635aa6c0901a591c638cc5ae0b7fe2
littleflute <13699175164@sina.cn>
2018-02-07 16:16:18 +0800

    Update c1.js

--- Issues/6/c1.js
+++ Issues/6/c1.js
@@ -12,15 +12,63 @@ function api(b,d)
 	}
 	_on_off_div(b,d.v);
 }
+
 function _blJobClass(p)
 { 
-	this.v = "v0.0.22";
+	this.v = "v0.0.51";
 	var ta0 = null;
 	var ta1 = null;
 	var ta2 = null; 
+	var fs = [];
+	var _v4Files = null;
+	var _strPath = null;
+	var _pic = null;
+	var ff = function(){
+		var a = ta1.value.split(".mp3,");
+		blo0.blShowObj2Div(_v4Files.d,a);
+		var s = '[aplayer autoplay="true"]';
+		var b = a[a.length-1].split(".jpg,"); 
+		_pic = _strPath + "/" + b[1].replace("\n","");
+
+		for(var i = 0; i < a.length - 1 ; i++){
+			var _title = a[i].replace("\n","");
+			s += '\n[mp3 ';
+			s += ' url = "';
+			s += _strPath + "/" + _title;
+			s += '.mp3"';
+
+			s += ' pic = "';
+			s += _pic;
+			s += '.jpg"';
+
+			s += ' lrc="https://littleflute.github.io/Songs/mp3/%E6%88%91%E7%9A%84%E6%98%8E%E5%A4%A9%20-%20%E9%83%91%E6%99%BA%E5%8C%96.lrc"';
+			
+			s += ' title = "';
+			s += _title;
+			s += '" artist = "BobMarley" ';
+
+			s += '] [/mp3]\n';
+		}
+		s += '[/aplayer]';
+		ta2.value = s;
+	};
 	this.blrRun = function(b,d){
 		b.style.backgroundColor = blGrey[0];
 		if(ta0) eval(ta0.value);
+	}
+	this.blrSetPath = function(b,d){
+		_strPath = ta1.value;
+	}
+	this.blrGetFiles = function(b,d){
+		if(!d.v) {
+			_v4Files = d.v = blo0.blMDiv(document.body,d.id+"_v","files",110,50,800,100,blGrey[1]);
+			d.v.d = blo0.blDiv(d.v,d.v.id+"d","d:",blGrey[2]);
+		}
+		ff();_on_off_div(b,d.v);
+
+	}
+	this.blrCode4Files = function(b,d){
+		ta0.value = ff;
 	}
 	this.blrTA0 = function(b,d){
 		if(!d.ta) {ta0 = d.ta = blo0.blTextarea(d,d.id+"_ta","",blGrey[0]);d.ta.style.width="95%";}
