97160b574909b75ecf61443c89d3f3e4f4030dc4
littleflute <13699175164@sina.cn>
2018-02-16 05:31:32 +0800

    Update 2.js

--- js/playerList/2.js
+++ js/playerList/2.js
@@ -1,6 +1,6 @@
 
 // 2.js
-var s = 'playerList_2_V0.0.13';
+var s = 'playerList_2_V0.0.14';
 s += '<a href="https://github.com/littleflute/beautifullover.org/edit/master/js/playerList/2.js" target="_blank">2.js*</a> - '; 
 s += '<a href="https://littleflute.github.io/beautifullover.org/js/playerList/2.js" target="_blank">2.js</a>';
 
@@ -12,23 +12,23 @@ var m = {
         type: 'file',
         source: [ 
 {
-	name: 'Three Little Birds',
-	author: 'BobMarley',
-	src: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/01%20Three%20Little%20Birds.mp3',
-	cover: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/AlbumArt_{7666695E-5B42-46D5-9567-CA01B5E97B32}_Large.jpg'
+	name: 'Black dog/Led Zeppelin IV',
+	author: 'Led Zeppelin',
+	src: 'https://littleflute.github.io/Led-Zeppelin1/152/cd1/01_曲目 1.mp3',
+	cover: 'https://littleflute.github.io/Led-Zeppelin1/152/index.jpg'
 },
 {
-	name: '02 Redemption Song.mp3',
-	author: 'BobMarley',
-	src: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/02 Redemption Song.mp3',
-	cover: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/AlbumArt_{7666695E-5B42-46D5-9567-CA01B5E97B32}_Large.jpg'
+	name: 'Rock and roll / Led Zeppelin IV',
+	author: 'Led Zeppelin',
+	src: 'https://littleflute.github.io/Led-Zeppelin1/152/cd1/02_曲目 2.mp3',
+	cover: 'https://littleflute.github.io/Led-Zeppelin1/152/index.jpg'
 },
 
 {
-	name: '03 Wake Up and Live, Pt. 1.mp3',
-	author: 'BobMarley',
-	src: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/03 Wake Up and Live, Pt. 1.mp3',
-	cover: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/AlbumArt_{7666695E-5B42-46D5-9567-CA01B5E97B32}_Large.jpg'
+	name: 'Led Zeppelin IV',
+	author: 'Led Zeppelin',
+	src: 'https://littleflute.github.io/Led-Zeppelin1/152/cd1/03_曲目 3.mp3',
+	cover: 'https://littleflute.github.io/Led-Zeppelin1/152/index.jpg'
 },
 
 
