39a9ab6e8c5b1dc447f402abae33d21d30f0c2c7
littleflute <13699175164@sina.cn>
2018-02-16 04:10:09 +0800

    Update 1.js

--- js/playerList/1.js
+++ js/playerList/1.js
@@ -1,7 +1,7 @@
 // 1.js
-var s = 'playerList_1_V0.0.4';
+var s = 'playerList_1_V0.0.5';
 s += '<a href="https://github.com/littleflute/beautifullover.org/edit/master/js/playerList/1.js" target="_blank">1.js*</a> - '; 
-s += '<a href="https://littleflute.github.io/beautifullover.orgjs/playerList/1.js" target="_blank">1.js</a>';
+s += '<a href="https://littleflute.github.io/beautifullover.org/js/playerList/1.js" target="_blank">1.js</a>';
 
 var listUI = blo0.blMDiv(document.body,"id_mdiv_ListUI",s,100,200,400,300,blGrey[5]);
 loadPlayerListOK();
