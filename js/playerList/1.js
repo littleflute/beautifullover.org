853441345b5452673dcf3f38ebed4959c13c382b
littleflute <13699175164@sina.cn>
2018-02-16 04:12:59 +0800

    Update 1.js

--- js/playerList/1.js
+++ js/playerList/1.js
@@ -1,7 +1,52 @@
 // 1.js
-var s = 'playerList_1_V0.0.11';
+var s = 'playerList_1_V0.0.12';
 s += '<a href="https://github.com/littleflute/beautifullover.org/edit/master/js/playerList/1.js" target="_blank">1.js*</a> - '; 
 s += '<a href="https://littleflute.github.io/beautifullover.org/js/playerList/1.js" target="_blank">1.js</a>';
 
 var listUI = blo0.blMDiv(document.body,"id_mdiv_ListUI",s,100,200,400,300,blGrey[5]);
+
+// list-------------------------------------
+var m = {
+        type: 'file',
+        source: [ 
+{
+	name: 'Three Little Birds',
+	author: 'BobMarley',
+	src: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/01%20Three%20Little%20Birds.mp3',
+	cover: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/AlbumArt_{7666695E-5B42-46D5-9567-CA01B5E97B32}_Large.jpg'
+},
+{
+	name: '02 Redemption Song.mp3',
+	author: 'BobMarley',
+	src: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/02 Redemption Song.mp3',
+	cover: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/AlbumArt_{7666695E-5B42-46D5-9567-CA01B5E97B32}_Large.jpg'
+},
+
+{
+	name: '03 Wake Up and Live, Pt. 1.mp3',
+	author: 'BobMarley',
+	src: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/03 Wake Up and Live, Pt. 1.mp3',
+	cover: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/AlbumArt_{7666695E-5B42-46D5-9567-CA01B5E97B32}_Large.jpg'
+},
+
+
+{
+	name: '04 Bend Down Low.mp3',
+	author: 'BobMarley',
+	src: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/04 Bend Down Low.mp3',
+	cover: 'https://littleflute.github.io/BobMarley/B%20Is%20for%20Bob/AlbumArt_{7666695E-5B42-46D5-9567-CA01B5E97B32}_Large.jpg'
+},
+
+]
+    };
+
+var p = {
+    autoplay: 1,
+    listshow: 0,
+    mode: 'listloop',    
+};
+p.music = m;
+
+// list=============================================
+
 loadPlayerListOK();
