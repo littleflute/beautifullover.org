aca468c59324ffbd1289314a46a8654d65579921
littleflute <13699175164@sina.cn>
2018-02-16 05:08:48 +0800

    Update index.js

--- js/playerList/index.js
+++ js/playerList/index.js
@@ -1,7 +1,9 @@
 // index.js 
 
 function _blPlayerListIndexClass(){
+  this.v = "v0.0.2";
 }
 
 var _wrap = bl$("id_mdiv_ListUI");
 _wrap.v = blo0.blDiv( _wrap,_wrap.id + "v","_blPlayerListIndexClass",blGrey[0]);
+blo0.blShowObj2Div(_wrap.v,new _blPlayerListIndexClass);
