9275d45b4cdd5066f1de541ed8baad53184312bf
littleflute <13699175164@sina.cn>
2018-02-16 05:14:03 +0800

    Update index.js

--- js/playerList/index.js
+++ js/playerList/index.js
@@ -4,10 +4,10 @@ function _blPlayerListIndexClass(){
   this.v = "v0.0.11";
   this.blrAboutMe = function(b,d){
     if(!d.v){
-      var s = 'playerList_1_V0.0.13';
+      var s = 'playerList_1_V0.0.14';
       s += '<a href="https://github.com/littleflute/beautifullover.org/edit/master/js/playerList/index.js" target="_blank">index.js*</a> - '; 
       s += '<a href="https://littleflute.github.io/beautifullover.org/js/playerList/index.js" target="_blank">index.js</a>';
-      d.v = blo0.blDiv(d,d.id+"v","v",blGrey[1]);
+      d.v = blo0.blDiv(d,d.id+"v",s,blGrey[1]);
     }
     _on_off_div(b,d.v);
   }
