d4e4f80b9b6cce1180585f00db0a7d10727393a3
littleflute <13699175164@sina.cn>
2018-02-16 13:12:14 +0800

    Update blMp3Player.js

--- js/blMp3Player.js
+++ js/blMp3Player.js
@@ -1,5 +1,5 @@
 //blMp3Player.js
-var _v_blMp3Player = "v0.0.34";
+var _v_blMp3Player = "v0.0.35";
 var s = _v_blMp3Player;
 s += '<a href="https://github.com/littleflute/beautifullover.org/edit/master/js/blMp3Player.js" target="_blank">blMp3Player.js*</a> - '; 
 s += '<a href="https://littleflute.github.io/beautifullover.org/js/blMp3Player.js" target="_blank">blMp3Player.js</a>';
@@ -8,10 +8,14 @@ var playerUI = blo0.blMDiv(document.body,"id_mdiv_playerUI",s,50,50,400,300,blGr
 
 function _blMp3PlayerClass(p){
   this.v = _v_blMp3Player;
-  
+  this. blrSongList = function(b,d){
+    if(!d.v){
+      d.v = blo0.blDiv(d,d.id+"v","v",blGrey[0]);
+    }
+    _on_off_div(b,d.v);
+  }
   var _wrap = playerUI;
   _wrap.v = blo0.blDiv( _wrap,_wrap.id + "v","p",blGrey[0]);
-  blo0.blShowObj2Div(_wrap.v,p.music);
-  
+  blo0.blShowObj2Div(_wrap.v,p.music.source);  
   
 }
