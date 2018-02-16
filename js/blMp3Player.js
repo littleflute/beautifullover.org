c0fc6f6bd8c43f5c20c55dcdf0e97fd971ab9d0d
littleflute <13699175164@sina.cn>
2018-02-16 12:59:07 +0800

    Update blMp3Player.js

--- js/blMp3Player.js
+++ js/blMp3Player.js
@@ -1,1 +1,7 @@
 //blMp3Player.js
+var s = 'blMp3Player_V0.0.22';
+s += '<a href="https://github.com/littleflute/beautifullover.org/edit/master/js/blMp3Player.js" target="_blank">blMp3Player.js*</a> - '; 
+s += '<a href="https://littleflute.github.io/beautifullover.org/js/blMp3Player.js" target="_blank">blMp3Player.js</a>';
+
+var playerUI = blo0.blMDiv(document.body,"id_mdiv_playerUI",s,50,50,400,300,blGrey[6]); 
+
