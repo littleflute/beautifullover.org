2a714b34fc0487d738f97a0002e3b146bb84cc16
littleflute <13699175164@sina.cn>
2018-02-16 15:15:35 +0800

    Update blMp3Player.js

--- js/blMp3Player.js
+++ js/blMp3Player.js
@@ -1,11 +1,17 @@
 //blMp3Player.js
-var _v_blMp3Player = "v0.0.144";
+var _v_blMp3Player = "v0.0.151";
 var s = _v_blMp3Player;
 s += '<a href="https://github.com/littleflute/beautifullover.org/edit/master/js/blMp3Player.js" target="_blank">blMp3Player.js*</a> - '; 
 s += '<a href="https://littleflute.github.io/beautifullover.org/js/blMp3Player.js" target="_blank">blMp3Player.js</a>';
 
 var playerUI = blo0.blMDiv(document.body,"id_mdiv_playerUI",s,50,50,400,300,blGrey[6]); 
  
+function loadLyricOK(){ 
+	 var l = document.getElementById("lrcdata");
+	 var lDiv = blo0.blMDiv(document.body,"id_lDiv","lDiv",550,50,500,400,blGrey[6]);
+	 lDiv.v = blo0.blDiv(lDiv,lDiv.id + "v",l.innerHTML,blGrey[9]);
+}
+
 function _parseSource(_a,_div,_src){
   var d = _div;
   d.d0 = blo0.blDiv(d,d.id+"d0" ,"d0",blGrey[7]);   
@@ -59,9 +65,21 @@ function _blMp3PlayerClass(p){
     _on_off_div(b,d.v);
   }
   
+  this. blrLoadLrc = function(b,d){
+    if(!d.v){
+      d.v = blo0.blDiv(d,d.id+"v","v",blGrey[0]);   
+      var myLrc = "js/Lrc/1.js";
+      if(myLrc) blo0.blScript("id_script_lrcdata",myLrc);
+    }
+    _on_off_div(b,d.v);
+  }
+  
   var _wrap = playerUI;
   _wrap.v = blo0.blDiv( _wrap,_wrap.id + "v","p",blGrey[0]);
   blo0.blShowObj2Div(_wrap.v,this);  
   bl$("blrAudio").click();
   bl$("blrSongList").click();
 }
+
+
+
